9e32339ca39feeffe4ee33a198b7746755b4054e
hadiza13 <hadiza13@stanford.edu>
2019-11-14 00:00:06 -0800

    ROUTINE COMMIT

--- FrameworkCode/Fan.c
+++ FrameworkCode/Fan.c
@@ -32,62 +32,50 @@ void Fan(void)
 ADC_MultiInit(1);//0-3.3V, PE0
 //Array to store POT input
 
-
 uint32_t Pot_ConversionResults[1];
 //Initialize PWM module on specific bits
 
 	bool i;
 
-i = PWM_TIVA_Init(7); //Jad already initialized this, Use PB4 for fan motor
+i = PWM_TIVA_Init(7); //Jad already initialized this, Use PC4 for fan motor
 
 //if we set to goup 1 will it change to freq for both PB4 and PB5?
 //Not if you dont Initialize PB5 as PWM
-//Need to experimentally find right frequency
-
+//Need to experimentally find right frequency - ENDED UP USING 63000 PERIOD
 
 //for spinner start until game end continuously read the below 
 //(I guess the change in output voltage of the POT would be an event? 
-//No, the tiva would just be cintinuously reading this throughout the game, 
+//No, the tiva would just be cOntinuously reading this throughout the game, 
 //it wouldnt be an event, it would just be a while loop)
 
+uint32_t pot_voltage;// TO READ THE VOLTAGE AT THE POT OUTPUT AND MAP IT TO THE 0-4093 RANGE
 
-uint32_t pot_voltage;
-
-//I can only have max 100 possible duty cycle values, 
-//need to saegment 4093 into 100 tranches and let that correspond to different duty cycle values
-
-
-uint32_t pulse_width;
-
-//	Duty_Cycle = abs((pot_voltage)*(100/4093));
-		
-
+uint32_t pulse_width;//TO STORE PULSE WIDTH
 		
 //	PWM_TIVA_SetDuty(duty_cycle[98],7);
-	printf("while start\r\n");
+//	printf("while start\r\n");
 
 while(kbhit()!=1)
 //use kbhit() in while loop to test
 {
+	//READ VOLTAGE AT POT OUTPUT
 	ADC_MultiRead(Pot_ConversionResults);
 	
+	//STORE POT OUTPUT "VOLTAGE VALUE" IN VARIABLE FROM ARRAY
 	pot_voltage = Pot_ConversionResults[0];
-	printf("Pot Voltage = %u\r\n",pot_voltage);
-	
-
+//SET FREQUENCY
 	PWM_TIVA_SetPeriod( 63000, 3);
+	
+	//MAPPING PULSE WIDTH TO POT OUTPUT VOLTAGE
 	pulse_width = abs((pot_voltage)*(63000/4093));
 	
+	//ASSIGNING PULSE WIDTH VALUE TO OUTPUT TO GATE OF MOSFET AND CONTROL FAN SPEED
 	PWM_TIVA_SetPulseWidth( pulse_width,6);
-	
-	
-		
-	printf("pulse_width = %u\r\n",pulse_width);
-		
-//	PWM_TIVA_SetDuty(Duty_Cycle,7);
 }
 
 }
+
+//TEST HARNESS
 #ifdef TEST
 #include "termio.h"
 
